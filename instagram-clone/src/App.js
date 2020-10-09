137523882f72416d0e0352f4076cc6a3a7c1f586
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-08 17:22:51 -0700

    Setup firebase database

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
+import { db } from './firebase';
 
 function App() {
   const [posts, setPosts] = useState([
-    {
-      username: "stephen", 
-      caption: "wow it works", 
-      imageUrl: "https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-    },
-    {
-      username: "stephen", 
-      caption: "juicy", 
-      imageUrl: "https://images.unsplash.com/photo-1485856407642-7f9ba0268b51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    }
+    
   ]);
+// {
+    //   username: "stephen", 
+    //   caption: "wow it works", 
+    //   imageUrl: "https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    // },
+    // {
+    //   username: "stephen", 
+    //   caption: "juicy", 
+    //   imageUrl: "https://images.unsplash.com/photo-1485856407642-7f9ba0268b51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    // }
+    
+  // useEffect - runs a piece of code based on a specifc condition 
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      // every time a new post is added, this code fires 
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+    
+  }, []); 
 
   return (
     <div className="app">
@@ -30,8 +44,8 @@ function App() {
 
       {/* {Posts} */}
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
       
