de87a06dcbf2d5e99af7d64bef0052556a885366
stephengogo <44884901+stephengogo@users.noreply.github.com>
2020-10-09 17:30:56 -0700

    InstagramEmbed

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -6,6 +6,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
 import ImageUpload from './ImageUpload';
+import InstagramEmbed from 'react-instagram-embed';
+
+
 
 function getModalStyle() {
   const top = 50;
@@ -185,14 +188,36 @@ function App() {
         )}
       </div>
 
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {/* {Posts} */}
+          {
+            posts.map(({id, post}) => (
+              <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+            ))
+          }
+        </div>
+        <div className="app__postsRight">
+          <InstagramEmbed
+          url='https://www.instagram.com/p/CFPOqPGjV08/'
+          maxWidth={320}
+          hideCaption={false}
+          containerTagName='div'
+          protocol=''
+          injectScript
+          onLoading={() => {}}
+          onSuccess={() => {}}
+          onAfterRender={() => {}}
+          onFailure={() => {}}
+          />
+        </div>
+        
+      </div>
       
 
-      {/* {Posts} */}
-      {
-        posts.map(({id, post}) => (
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        ))
-      }
+      
+
+      
       
       {user?.displayName ? (
         <ImageUpload username={user.displayName}/>
